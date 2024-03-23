1879443131d6676b557ad275b8728908fba86ecc
870712pokohu <870712pokohu@gmail.com>
2024-03-23 03:09:49 -0400

    create post with ajax and add alert to check if post is successfully created

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse, HttpRequest
-
-
+from .forms import PostForm
+from profiles.models import Profile
 # Create your views here.
 
 def is_ajax(request):
@@ -10,28 +10,46 @@ def is_ajax(request):
 
 
 def past_list_and_create(request):
-  qs = Post.objects.all()
-  return render(request, 'posts/main.html', {'qs':qs})
+  form = PostForm(request.POST or None)
+  #qs = Post.objects.all()
+  if is_ajax(request):
+    if form.is_valid():
+      author = Profile.objects.get(user = request.user)
+      instance = form.save(commit = False)
+      instance.author = author
+      instance.save()
+      return JsonResponse({
+        'title': instance.title,
+        'body': instance.body,
+        'author':instance.author.user.username,
+        'id':instance.id,
+      })
+  context = {
+    'form': form,
+  }
+
+  return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
-  visible = 3
-  upper = num_posts
-  lower = upper - visible
-  size = Post.objects.all().count()
+  if is_ajax(request):
+    visible = 3
+    upper = num_posts
+    lower = upper - visible
+    size = Post.objects.all().count()
 
-  qs = Post.objects.all()
-  data = []
-  for obj in qs:
-    item = {
-      'id': obj.id,
-      'title': obj.title,
-      'body': obj.body,
-      'liked': True if request.user in obj.liked.all() else False,
-      'count': obj.like_count,
-      'author': obj.author.user.username
-    }
-    data.append(item)
-  return JsonResponse({'data':data[lower:upper], 'size': size})
+    qs = Post.objects.all()
+    data = []
+    for obj in qs:
+      item = {
+        'id': obj.id,
+        'title': obj.title,
+        'body': obj.body,
+        'liked': True if request.user in obj.liked.all() else False,
+        'count': obj.like_count,
+        'author': obj.author.user.username
+      }
+      data.append(item)
+    return JsonResponse({'data':data[lower:upper], 'size': size})
 
 def like_unlike_post(request: HttpRequest):
   if is_ajax(request):
