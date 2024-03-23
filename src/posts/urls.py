9cc74cfc1a261b88a31a2d25d60862819a25a294
870712pokohu <870712pokohu@gmail.com>
2024-03-23 00:07:10 -0400

    configure liked button with ajax call to change the render effect

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -3,6 +3,7 @@ from .views import (
   past_list_and_create,
   load_post_data_view,
   hello_world_view,
+  like_unlike_post
 )
 
 app_name = 'posts'
@@ -10,5 +11,6 @@ app_name = 'posts'
 urlpatterns = [
   path('', past_list_and_create, name='main-board'),
   path('data/<int:num_posts>', load_post_data_view, name='posts-data'),
+  path('like-unlike/', like_unlike_post, name='like-unlike'),
   path('hello-world/', hello_world_view, name='hello-world')
 ]
